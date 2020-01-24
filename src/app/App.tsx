6bebcc9549b51b446148e6e70b9e28d737756ae5
Chris Camel <camel.christophe@gmail.com>
2020-01-24 15:42:41 +0100

    replace char by equivalent html entity

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -93,8 +93,8 @@ const Header = () => {
       <Typography variant="h6" noWrap className={classes.toolbarTitle}>Company name</Typography>
       <nav>
         <Link to="/" exact color="textSecondary" className={classes.link} component={NavLink} activeClassName={classes.selected}>Current usage</Link>
-        <Link to="/multicluster-view" color="textSecondary" className={classes.link} component={NavLink} activeClassName={classes.selected}>Consolidated usage & History</Link>
-        <Link to="/cluster-view" color="textSecondary" className={classes.link} component={NavLink} activeClassName={classes.selected}>Usage trends & Accounting</Link>
+        <Link to="/multicluster-view" color="textSecondary" className={classes.link} component={NavLink} activeClassName={classes.selected}>Consolidated usage &amp; History</Link>
+        <Link to="/cluster-view" color="textSecondary" className={classes.link} component={NavLink} activeClassName={classes.selected}>Usage trends &amp; Accounting</Link>
       </nav>
       <Button href="#" variant="outlined" className={classes.link}>
         Login
