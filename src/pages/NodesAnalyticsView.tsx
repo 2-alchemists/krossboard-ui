70fcf737f0eb6f858cdb24f2e644746ba7871c40
Chris Camel <camel.christophe@gmail.com>
2021-04-08 19:02:57 +0200

    refactor: split component
    Signed-off-by: Chris Camel <camel.christophe@gmail.com>

--- src/pages/NodesAnalyticsView.tsx
+++ src/pages/NodesAnalyticsView.tsx
@@ -38,19 +38,17 @@ const useStyles = makeStyles(theme => ({
     dashboardForm: {
         minWidth: '14rem'
     },
-    grid: {
-    }
+    grid: {}
 }))
 
 enum DashboardTypes {
     NodesRecentOccupation = "Nodes' recent occupation",
-    NodesUsageHistory = "Nodes' usage history",
+    NodesUsageHistory = "Nodes' usage history"
 }
 
 export const NodesAnalyticsView = () => {
     const classes = useStyles()
     const store = useStore()
-
     const [selectedCluster, setSelectedCluster] = React.useState(store.clusterNames.length === 0 ? '' : store.clusterNames[0])
     const onClusterChange = (event: React.ChangeEvent<{ value: unknown }>) => {
         setSelectedCluster(event.target.value as string)
@@ -67,76 +65,86 @@ export const NodesAnalyticsView = () => {
         }
     })
 
+    return (
+        <div className={classes.root}>
+            <Grid container>
+                <Grid item className={classes.centeredGrid} xs={12} sm={12} md={12}>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="cluster-label">cluster</InputLabel>
+                        <Select labelId="cluster-label" id="cluster" value={selectedCluster} onChange={onClusterChange}>
+                            {store.clusterNames
+                                .filter((it, index, self) => self.indexOf(it) === index)
+                                .map(it => (
+                                    <MenuItem key={it} value={it}>
+                                        {it}
+                                    </MenuItem>
+                                ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="dashboard-label">dashboard</InputLabel>
+                        <Select
+                            className={classes.dashboardForm}
+                            labelId="dashboard-label"
+                            id="dashboard"
+                            value={selectedDashboard}
+                            onChange={onDashboardChange}
+                        >
+                            {Object.keys(DashboardTypes).map(it => (
+                                <MenuItem key={it} value={it}>
+                                    {DashboardTypes[it as keyof typeof DashboardTypes]}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Grid>
+            </Grid>
+            <NodesRecentOccupationView selectedCluster={selectedCluster}/>
+        </div>
+    )
+}
+
+const NodesRecentOccupationView: React.FC<{selectedCluster: string}> = ({selectedCluster}) => {
+    const classes = useStyles()
+    const store = useStore()
+
     return useObserver(() => {
         const data = store.currentNodesLoad[selectedCluster] ? store.currentNodesLoad[selectedCluster].data : {}
-        const color = (value?: IUsageHistoryItem, idx?: number) => (
-            seriesColorSchema[ (idx || 0) % seriesColorSchema.length]
-        )
+        const color = (value?: IUsageHistoryItem, idx?: number) => seriesColorSchema[(idx || 0) % seriesColorSchema.length]
 
         return (
-            <div className={classes.root}>
-                <Grid container>
-                    <Grid item className={classes.centeredGrid} xs={12} sm={12} md={12}>
-                        <FormControl className={classes.formControl}>
-                            <InputLabel id="cluster-label">cluster</InputLabel>
-                            <Select labelId="cluster-label" id="cluster" value={selectedCluster} onChange={onClusterChange}>
-                                {store.clusterNames
-                                    .filter((it, index, self) => self.indexOf(it) === index)
-                                    .map(it => (
-                                        <MenuItem key={it} value={it}>
-                                            {it}
-                                        </MenuItem>
-                                    ))}
-                            </Select>
-                        </FormControl>
-                        <FormControl className={classes.formControl}>
-                            <InputLabel id="dashboard-label">dashboard</InputLabel>
-                            <Select  className={classes.dashboardForm} labelId="dashboard-label" id="dashboard" value={selectedDashboard} onChange={onDashboardChange}>
-                                {Object.keys(DashboardTypes)
-                                    .map(it => (
-                                        <MenuItem key={it} value={it}>
-                                            {DashboardTypes[it as keyof typeof DashboardTypes]}
-                                        </MenuItem>
-                                    ))}
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={3}>
-                    {
-                    Object.keys(data)                 
-                      .map(nodeName => {
-                          const usages = data[nodeName]
-                       
-                        return (
-                            <React.Fragment key={nodeName}>
-                                <Grid item xs={12} sm={6} md={3} className={classes.grid}>
-                                    <CurrentLoad
-                                        resourceName={nodeName}
-                                        resourceType="CPU"
-                                        outToDate={false}
-                                        data={usages}
-                                        nameKey="tag"
-                                        dataKey="cpuUsage"
-                                        color={color}
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6} md={3} className={classes.grid}>
-                                    <CurrentLoad
-                                        resourceName={nodeName}
-                                        resourceType="Memory"
-                                        outToDate={false}
-                                        data={usages}
-                                        nameKey="tag"
-                                        dataKey="memUsage"
-                                        color={color}
-                                    />
-                                </Grid>
-                            </React.Fragment>
-                        )
-                    })}
-                </Grid>
-            </div>
+            <Grid container spacing={3}>
+                {Object.keys(data).map(nodeName => {
+                    const usages = data[nodeName]
+
+                    return (
+                        <React.Fragment key={nodeName}>
+                            <Grid item xs={12} sm={6} md={3} className={classes.grid}>
+                                <CurrentLoad
+                                    resourceName={nodeName}
+                                    resourceType="CPU"
+                                    outToDate={false}
+                                    data={usages}
+                                    nameKey="tag"
+                                    dataKey="cpuUsage"
+                                    color={color}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={3} className={classes.grid}>
+                                <CurrentLoad
+                                    resourceName={nodeName}
+                                    resourceType="Memory"
+                                    outToDate={false}
+                                    data={usages}
+                                    nameKey="tag"
+                                    dataKey="memUsage"
+                                    color={color}
+                                />
+                            </Grid>
+                        </React.Fragment>
+                    )
+                })}
+            </Grid>
         )
     })
 }
