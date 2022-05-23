5d1f368c314dd559e6ae53931c63d8c28c7e5eb4
Christophe Camel <camel.christophe@gmail.com>
2022-05-23 21:23:56 +0200

    chore: apply license header

--- src/component/HistoryChart.tsx
+++ src/component/HistoryChart.tsx
@@ -1,3 +1,16 @@
+/*
+  Copyright (C) 2020  2ALCHEMISTS SAS.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 import { format } from 'date-fns'
 import { TFunction } from 'i18next'
 import { useObserver } from 'mobx-react-lite'
